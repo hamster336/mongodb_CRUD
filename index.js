e90df487f5ae4a8334e0362ecf6bb2ff8eb31bc7
hamster336 <aashukalauni12@gmail.com>
2026-05-29 14:08:03 +0545

    get API for single product with id

--- index.js
+++ index.js
@@ -14,16 +14,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
 });
 
-// add product
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 // get all products
 app.get("/api/products", async (req, res) => {
   try {
@@ -34,6 +24,33 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+// get single product by id
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+// add product
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// connect to DB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
